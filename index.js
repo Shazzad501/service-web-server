a4fd2f3d07425aa4013a5c992432fa8db36b213b
Shazzad501 <assazzad501@gmail.com>
2024-12-23 20:17:38 +0600

    spacific service collect by id api done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -57,6 +57,14 @@ async function run() {
       const cursor = servicesCollection.find().limit(6)
       const result= await cursor.toArray();
       res.send(result)
+    })
+
+    // get only one service call by id
+    app.get('/services/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
     })
   } finally {
     // Ensures that the client will close when you finish/error
